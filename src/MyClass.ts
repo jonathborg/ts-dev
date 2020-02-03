101b0ed30b71ee03c66e8f81ed325c76612fa852
Jonathan Borges <jonathan.borges@rededompedro.com>
2020-02-03 12:00:58 -0300

    Revert "Initial commit"
    This reverts commit 86654ef0110243cc3cc88107006fcb4b524223e5.

---
+++ src/MyClass.ts
@@ -0,0 +1,12 @@
+export default class MyClass {
+
+    something: string;
+
+    constructor(something: string) {
+        this.something = something;
+    }
+
+    getSomething() {
+        return this.something;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import { DateTime } from 'luxon';
+Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, 800);
+import MyClass from './MyClass';
 
-export function toDateId(date: Date) {
-    return DateTime.fromJSDate(date).toFormat('yyyyMMddHHmmss');
-}
+(async () => {
+
+    const myClass = new MyClass('Hello World');
+    console.log(myClass.getSomething());
+
+    debugger;
+})()
+.catch(console.error);
